a3249b804366ea1b7c27a868f5f6270d27474406
Josh Lucas <jclucas21@gmail.com>
2017-04-26 17:15:13 -0500

    refactored to use duck type

--- chapter-5.rb
+++ chapter-5.rb
@@ -3,28 +3,41 @@ class Trip
 
   def prepare(preparers)
     preparers.each do |preparer|
-      case preparer
-      when Mechanic
-        preparer.prepare_bicycles(bicycles)
-      when TripCoordinator
-        preparer.buy_food(customers)
-      when Driver
-        preparer.gas_up(vehicle)
-        preparer.fill_water_tank(vehicle)
-      end
+      preparer.prepare_trip(self)
     end
   end
 
 end
 
+class Mechanic
+
+  def prepare_trip(trip)
+    prepare_bicycles(trip.bicycles)
+  end
+
+  def prepare_bicycles(bicycles)
+    # .............
+  end
+end
+
 class TripCoordinator
-  def buy_food
+  def prepare_trip(trip)
+    self.buy_food(trip.customers)
+  end
+
+  def buy_food()
     # .............
   end
 end
 
 class Driver
-  def gas_up
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    self.gas_up(vehicle)
+    self.fill_water_tank(vehicle)
+  end
+
+  def gas_up(vehicle)
     # .............
   end
 
