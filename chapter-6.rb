84217ac23bf90c61b125658f21dda05437ee1780
Mithridates01 <jclucas21@gmail.com>
2017-04-30 13:36:01 -0500

    setup spares method for refactoring

--- chapter-6.rb
+++ chapter-6.rb
@@ -14,6 +14,11 @@ class Bycicle
 			"This #{Self.class} cannot respond to:"
 	end
 
+	def spares
+		{ tire_size: tire_size,
+		  chain:     chain }
+	end
+
 	def default_chain
 		"10-speed"
 	end
@@ -26,10 +31,20 @@ class MountainBike
 		@tire_size = args[:tire_size] || default_tire_size
 	end
 
+	def spares
+		super.merge( {rear_shock: rear_shock} )
+	end
+
+	def rear_shock
+		"ultra-shock"
+	end
+
 	def default_tire_size
 		"2.1"
 	end
 end
+
+
 
 class RoadBike
 	def initialize(args)
@@ -38,10 +53,22 @@ class RoadBike
 		@tire_size = args[:tire_size] || default_tire_size
 	end
 
+	def spares
+		{ chain: 		  "10-speed",
+			tire_size:  "23",
+			tape_color: tape_color }
+	end
+
+	def tape_color
+		"silver"
+	end
+
 	def default_tire_size
 		"23"
 	end
 end
+
+
 
 class RecumbentBike
 	def initialize(args)
